fc4592ffbee83fc32716365658543471b7e14ed7
sangeethjohn07 <sangeethjohn1997@gmail.com>
2020-05-29 22:49:20 +0530

    first commit

---
+++ routes/home.js
@@ -0,0 +1,178 @@
+const { Router } = require('express')
+const passport = require('passport')
+const { google } = require('googleapis')
+const KEYS = require('../configs/keys')
+var http = require('http');
+var fs = require('fs');
+const fetch = require('node-fetch');
+const fileType = require('file-type');
+var mime = require('mime-types');
+var requestify = require('requestify');
+
+const router = Router()
+
+router.get('/', function(req, res) {
+    res.render('home.html', { 'title': 'Application Home' })
+})
+
+router.get('/dashboard', function(req, res) {
+
+    // if not user
+    if (typeof req.user == "undefined") res.redirect('/auth/login/google')
+    else {
+
+        let parseData = {
+            title: 'DASHBOARD',
+            googleid: req.user._id,
+            name: req.user.name,
+            avatar: req.user.pic_url,
+            email: req.user.email
+        }
+
+        // if redirect with google drive response
+        if (req.query.file !== undefined) {
+
+            // successfully upload
+            if (req.query.file == "upload") parseData.file = "uploaded"
+            else if (req.query.file == "notupload") parseData.file = "notuploaded"
+        }
+
+        res.render('dashboard.html', parseData)
+    }
+})
+
+router.post('/upload', function(req, res) {
+
+    // not auth
+    if (!req.user) res.redirect('/auth/login/google')
+    else {
+        // auth user
+
+        // config google drive with client token
+        const oauth2Client = new google.auth.OAuth2()
+        oauth2Client.setCredentials({
+            'access_token': req.user.accessToken
+        });
+
+        const drive = google.drive({
+            version: 'v3',
+            auth: oauth2Client
+        });
+
+        // d = req.files.file_upload;
+        // console.log(d)
+        var url1 = req.body.url1;
+        console.log(url1);
+        var dest = url1.substring(url1.lastIndexOf('/') + 1);
+
+
+        function download(url1, dest, callback) {
+            var file = fs.createWriteStream(dest);
+            var request = http.get(url1, function(response) {
+                response.pipe(file);
+
+                file.on('finish', function() {
+                    file.close(callback); // close() is async, call callback after close completes.
+                });
+                file.on('error', function(err) {
+                    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+                    if (callback)
+                        callback(err.message);
+                });
+            });
+
+        };
+        download(url1, dest, function(err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("Download complete");
+                filename = dest;
+                mimetype = mime.lookup(dest);
+                // filename='abc';
+
+                const driveResponse = drive.files.create({
+                    requestBody: {
+                        name: filename,
+                        mimeType: mimetype
+                    },
+                    media: {
+                        mimeType: mimetype,
+                        body: fs.createReadStream(dest)
+                    }
+                });
+
+                driveResponse.then(data => {
+
+                    if (data.status == 200) res.redirect('/dashboard?file=upload') // success
+                    else res.redirect('/dashboard?file=notupload') // unsuccess
+
+                }).catch(err => { throw new Error(err) })
+
+            }
+        });
+        //move file to google drive
+        var dataq, data1;
+        var body;
+        // http.get(url1, function(response) {
+        //     //response.getBody();
+        //     var res = response;
+        //     var bodyChunks = [];
+        //     res.on('data', function(chunk) {
+        //         // You can process streamed parts here...
+        //         bodyChunks.push(chunk);
+        //     }).on('end', function() {
+        //         body = Buffer.concat(bodyChunks).toString();
+        //         // console.log('BODY: ' + body);
+        //     })
+
+        // });
+
+        // requestify.get(url1)
+        //     .then(function(response) {
+        //         // Get the response body (JSON parsed or jQuery object for XMLs)
+        //         // response.getBody();
+        //         body = response.body;
+
+        //     });
+
+
+        //console.log(fs.createReadStream(dest));
+        // fetch(url1)
+        //     .then(res => res.buffer())
+        //     .then(buffer => {
+        //         dataq = buffer;
+        //         //console.log(dataq);
+        //         //  let { name: filename, mimetype, data } = dest;
+        //         data = dataq;
+        //         filename = dest;
+        //         mimetype = mime.lookup(dest);
+        //         // filename='abc';
+
+        //         const driveResponse = drive.files.create({
+        //             requestBody: {
+        //                 name: filename,
+        //                 mimeType: mimetype
+        //             },
+        //             media: {
+        //                 mimeType: mimetype,
+        //                 body: fs.createReadStream(dest)
+        //             }
+        //         });
+
+        //         driveResponse.then(data => {
+
+        //             if (data.status == 200) res.redirect('/dashboard?file=upload') // success
+        //             else res.redirect('/dashboard?file=notupload') // unsuccess
+
+        //         }).catch(err => { throw new Error(err) })
+
+        //     });
+
+
+    }
+})
+
+
+
+module.exports = router
